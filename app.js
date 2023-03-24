fd7520d8b77ffc5dbc66dc0cab5dba7ad7f9113c
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-03-24 08:18:05 +0300

    Dynamically styling with inline styling

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
-      number: 0,
+      boxSelected: false,
     };
   },
   methods: {
-    add(num) {
-      this.number += num;
-    },
-  },
-  computed: {
-    result() {
-      if (this.number < 37) {
-        return "Not there yet";
-      } else if (this.number > 37) {
-        return "Too much!";
-      }
-
-      return this.number;
-    },
-  },
-  watch: {
-    result() {
-      setTimeout(() => {
-        this.number = 0;
-      }, 5000);
+    boxSelect() {
+      this.boxSelected = !this.boxSelected;
     },
   },
 });
 
-app.mount("#assignment");
+app.mount("#styling");
