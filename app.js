9f0431c103de70abbe21916f8d2055835d2191eb
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-04-03 23:26:35 +0300

    Introducing components

--- app.js
+++ app.js
@@ -1,65 +1,59 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: "",
-      message: "Vue is great!",
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@mail.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@jmail.com",
+        },
+      ],
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
-    setText() {
-      this.message = this.currentUserInput;
-    },
-  },
-  beforeCreate() {
-    console.log("beforeCreate()");
-  },
-  created() {
-    console.log("created()");
-  },
-  beforeMount() {
-    console.log("beforeMount()");
-  },
-  mounted() {
-    console.log("mounted()");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate()");
-  },
-  updated() {
-    console.log("updated()");
-  },
-  beforeUnmount() {
-    console.log("beforeUnmount()");
-  },
-  unmounted() {
-    console.log("unmounted()");
   },
 });
 
+app.component("friend-contact", {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+`,
+});
+
 app.mount("#app");
-
-setTimeout(() => {
-  app.unmount();
-}, 3000);
-
-// const app2 = Vue.createApp({
-//   data() {
-//     return {
-//       message: "Vue is great!",
-//     };
-//   },
-//   methods: {
-//     saveInput(event) {
-//       this.currentUserInput = event.target.value;
-//     },
-//     setText() {
-//       this.message = this.$refs.userText.value;
-//       // console.log("this.$refs.userText", this.$refs.userText);
-//     },
-//   },
-// });
-
-// app2.mount("#app2");
