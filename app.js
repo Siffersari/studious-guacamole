811087e1a43208c17d651a4fee717f81d9eacee5
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-04-03 15:56:32 +0300

    Vue app lifecycles hooks

--- app.js
+++ app.js
@@ -13,25 +13,53 @@ const app = Vue.createApp({
       this.message = this.currentUserInput;
     },
   },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
+  },
 });
 
 app.mount("#app");
 
-const app2 = Vue.createApp({
-  data() {
-    return {
-      message: "Vue is great!",
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      this.message = this.$refs.userText.value;
-      // console.log("this.$refs.userText", this.$refs.userText);
-    },
-  },
-});
+setTimeout(() => {
+  app.unmount();
+}, 3000);
 
-app2.mount("#app2");
+// const app2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Vue is great!",
+//     };
+//   },
+//   methods: {
+//     saveInput(event) {
+//       this.currentUserInput = event.target.value;
+//     },
+//     setText() {
+//       this.message = this.$refs.userText.value;
+//       // console.log("this.$refs.userText", this.$refs.userText);
+//     },
+//   },
+// });
+
+// app2.mount("#app2");
