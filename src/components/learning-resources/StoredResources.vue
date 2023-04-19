dbdcf4d2795cc206959e5554f380c801ad7ef490
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-04-19 21:49:13 +0300

    Styling and more components

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.name"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from "./LearningResource.vue";
+
+export default {
+  components: {
+    LearningResource,
+  },
+  props: {
+    storedResources: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
