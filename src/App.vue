8023e119c2a5c23bbf2ae2fa7e425118158f5f12
Siffersari <33033576+Siffersari@users.noreply.github.com>
2023-04-20 00:52:03 +0300

    v-model&inputs

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
-  <TheHeader title="Remember Me" />
-  <TheResource />
+  <the-form></the-form>
 </template>
 
 <script>
-import TheResource from "./components/learning-resources/TheResource.vue";
-import TheHeader from "./components/layouts/TheHeader.vue";
+import TheForm from './components/TheForm.vue';
 
 export default {
   components: {
-    TheResource,
-    TheHeader,
-  },
-  data() {
-    return {};
-  },
-};
+    TheForm
+  }  
+}
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
-
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: sans-serif;
 }
 
 body {
   margin: 0;
+  background-color: #292929;
 }
 </style>
